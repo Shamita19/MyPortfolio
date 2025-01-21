b77b99b65c5411a97011767a29daf42d8e24d51a
Shamita19 <shamitanaidu9@gmail.com>
2025-01-21 02:13:33 -0800

    css file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,6 +17,8 @@ const Hero = () => {
           }}
         />
         </p>
+        
+        <p>Hi, I’m Shamita Naidu! I’m a tech enthusiast with a passion for solving real-world problems through AI and machine learning. With a strong foundation in Python, cloud platforms like AWS, and tools like Docker, I love tackling challenges head-on and delivering solutions that don’t just work—they shine. Oh, and I’m always looking for the next exciting project to push the boundaries of what technology can do! 🚀</p>
         <div className="social-icons">
           <a href="https://www.linkedin.com/in/shamita-naidu-7555101b9/" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin"></i>
